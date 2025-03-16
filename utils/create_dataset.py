aaf6c888a616028fce0399ba16dbcc8e917b6f21
Zhou Shuangfan <103800953+ZhouSF-628@users.noreply.github.com>
2025-03-16 17:12:56 +0800

    Update create_dataset.py

--- utils/create_dataset.py
+++ utils/create_dataset.py
@@ -6,21 +6,6 @@ import numpy as np
 from tqdm import tqdm
 
 
-def is_image_file(filename):
-    return any(filename.endswith(extension) for extension in ['.png', '.jpg', '.jpeg', '.PNG', '.JPG', '.JPEG'])
-
-
-def make_dataset(dir):
-    images = []
-    assert os.path.isdir(dir), '%s is not a valid directory' % dir
-
-    for root, _, fnames in sorted(os.walk(dir)):
-        for fname in fnames:
-            if is_image_file(fname):
-                path = os.path.join(root, fname)
-                images.append(path)
-    return images
-
 def np2tensor(numpy_array):
     # H, W, C -> C, H, W
     np_transpose = np.ascontiguousarray(numpy_array.transpose((2, 0, 1)))
